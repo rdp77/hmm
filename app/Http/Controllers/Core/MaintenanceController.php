55f00316e6795b280a2086fe35fab5e72ab9b8fb
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2022-06-30 01:47:01 +0700

    Data Type

--- app/Http/Controllers/Core/MaintenanceController.php
+++ app/Http/Controllers/Core/MaintenanceController.php
@@ -325,9 +325,9 @@ class MaintenanceController extends Controller
         $dataArr = [];
         for ($i = 1; $i <= 12; $i++) {
             if (isset($data[$i])) {
-                $dataArr[$i] = $data[$i];
+                $dataArr[$i] = (float)$data[$i];
             } else {
-                $dataArr[$i] = 0;
+                $dataArr[$i] = (float)0;
             }
         }
 
