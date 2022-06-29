dfdb083844ceec1515f2d3986a46370d160b281c
Moh Ravi Dwi Putra <ravidwiputra@gmail.com>
2022-06-30 02:23:50 +0700

    das

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -110,7 +110,7 @@ class APIController extends Controller
             if (isset($data[$i])) {
                 $dataArr[$i] = (float)$data[$i];
             } else {
-                $dataArr[$i] = number_format(0, 2);
+                $dataArr[$i] = (float)number_format(0, 2);
             }
         }
 
